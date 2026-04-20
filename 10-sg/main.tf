d55aef5a46da761837ce4b20cb1a5edfb9873b91
unknown <vikas.arisela@gmail.com>
2026-04-20 20:55:43 +0400

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -4,6 +4,7 @@ module "sg" {
     project_name = var.project_name
     environment = var.environment
     sg_name = var.sg_names[count.index]
-    description = "Created for ${var.sg_names[count.index]}"
+    sg_description = "Created for ${var.sg_names[count.index]}"
     vpc_id =   local.vpc_id
 }
+
