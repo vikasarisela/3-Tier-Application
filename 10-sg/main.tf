7342d0930edca255655c4e249b038e446b8f0528
unknown <vikas.arisela@gmail.com>
2026-04-23 16:34:54 +0400

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -8,15 +8,15 @@ module "sg" {
     vpc_id =   local.vpc_id
 }
 
-# Open port 80 on the frontend EC2 Security Group and allow traffic only from module.sg[11].sg_id (the ALB Security Group).
-resource "aws_security_group_rule" "frontendinstance_accpeting_from_alb" {
-   type = "ingress"
-  security_group_id = module.sg[9].sg_id   #frontend SG ID
-  source_security_group_id = module.sg[11].sg_id
-  from_port   = 80
-  protocol = "tcp"
-  to_port     = 80
-}
+# # Open port 80 on the frontend EC2 Security Group and allow traffic only from module.sg[11].sg_id (the ALB Security Group).
+# resource "aws_security_group_rule" "frontendinstance_accpeting_from_alb" {
+#    type = "ingress"
+#   security_group_id = module.sg[9].sg_id   #frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id
+#   from_port   = 80
+#   protocol = "tcp"
+#   to_port     = 80
+# }
 #“Allow HTTP traffic (port 80)”
 # “ALB forwards traffic to whatever port is defined in the target group”
 # {
