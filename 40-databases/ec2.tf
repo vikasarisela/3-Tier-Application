b4661b03fc77dbb16c953451df33e8dde1d351fe
unknown <vikas.arisela@gmail.com>
2026-04-26 21:37:53 +0400

    terraform

--- 40-databases/ec2.tf
+++ 40-databases/ec2.tf
@@ -202,8 +202,40 @@ resource "terraform_data" "mysql" {
 
 resource "aws_route53_record" "mongodb" {
   zone_id = data.aws_route53_zone.cloudskills.zone_id
-  name    = "cloudskills.fun"
+  name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = data.aws_route53_zone.cloudskills.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = data.aws_route53_zone.cloudskills.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = data.aws_route53_zone.cloudskills.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
+
 }
